2c1b261abf5ec194f1196b6954f561a63e937a56
guangyuzhang <13621160019@163.com>
2020-06-19 10:44:38 +0800

    implemented space_energy_input_item procedure

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import equipment_energy_input_item
 import equipment_energy_output_category
 import equipment_energy_output_item
 import space_energy_input_category
+import space_energy_input_item
 import store_energy_input_category
 import store_energy_input_item
 import tenant_energy_input_category
@@ -40,6 +41,8 @@ def main():
     Process(target=equipment_energy_output_item.main, args=(logger,)).start()
     # space energy input by energy categories
     Process(target=space_energy_input_category.main, args=(logger,)).start()
+    # space energy input by energy items
+    Process(target=space_energy_input_item.main, args=(logger,)).start()
     # store energy input by energy categories
     Process(target=store_energy_input_category.main, args=(logger,)).start()
     # store energy input by energy items
