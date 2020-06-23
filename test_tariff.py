713c67355d8361825022541c2b545f80f2e927ef
guangyuzhang <13621160019@163.com>
2020-06-23 19:19:47 +0800

    added get_energy_item_tariffs procedure

--- test_tariff.py
+++ test_tariff.py
@@ -1,37 +1,30 @@
-import logging
-from logging.handlers import RotatingFileHandler
 from datetime import datetime
 import tariff
 
 
 def main():
     """main"""
-    # create logger
-    logger = logging.getLogger('myems-aggregation')
-    logger.setLevel(logging.DEBUG)
-
-    # create file handler which logs even debug messages
-    fh = RotatingFileHandler('myems-aggregation.log', maxBytes=1024 * 1024, backupCount=0)
-    fh.setLevel(logging.ERROR)
-    # create console handler with a higher log level
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-    fh.setFormatter(formatter)
-    # add the handlers to logger
-    logger.addHandler(ch)
-    logger.addHandler(fh)
-
+    print('Testing get_energy_category_tariffs ...')
     cost_center_id = 1
     energy_category_id = 1
     start_date_time_utc = datetime.strptime('2020-06-29 16:00:00', '%Y-%m-%d %H:%M:%S')
     end_date_time_utc = datetime.strptime('2020-07-01 15:59:59', '%Y-%m-%d %H:%M:%S')
-    tariffs = tariff.get_tariffs(cost_center_id,
-                                 energy_category_id,
-                                 start_date_time_utc,
-                                 end_date_time_utc)
+    tariffs = tariff.get_energy_category_tariffs(cost_center_id,
+                                                 energy_category_id,
+                                                 start_date_time_utc,
+                                                 end_date_time_utc)
+    for k, v in sorted(tariffs.items()):
+        print(k, v)
+
+    print('Testing get_energy_item_tariffs ...')
+    cost_center_id = 1
+    energy_item_id = 1
+    start_date_time_utc = datetime.strptime('2020-06-29 16:00:00', '%Y-%m-%d %H:%M:%S')
+    end_date_time_utc = datetime.strptime('2020-07-01 15:59:59', '%Y-%m-%d %H:%M:%S')
+    tariffs = tariff.get_energy_item_tariffs(cost_center_id,
+                                             energy_item_id,
+                                             start_date_time_utc,
+                                             end_date_time_utc)
     for k, v in sorted(tariffs.items()):
         print(k, v)
 
