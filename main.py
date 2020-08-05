e84b23ff4d7cb41d6a9fca90351d96c728462f0e
guangyuzhang <13621160019@163.com>
2020-08-05 16:03:21 +0800

    implemented combined equipment billing procedures

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import logging
 from logging.handlers import RotatingFileHandler
 from multiprocessing import Process
 
+import combined_equipment_billing_input_category
+import combined_equipment_billing_input_item
+import combined_equipment_billing_output_category
 import equipment_billing_input_category
 import equipment_billing_input_item
 import equipment_billing_output_category
@@ -41,42 +44,56 @@ def main():
     # add the handlers to logger
     logger.addHandler(fh)
 
+    # combined equipment billing input by energy categories
+    Process(target=combined_equipment_billing_input_category.main, args=(logger,)).start()
+    # combined equipment billing input by energy items
+    Process(target=combined_equipment_billing_input_item.main, args=(logger,)).start()
+    # combined equipment billing output by energy categories
+    Process(target=combined_equipment_billing_output_category.main, args=(logger,)).start()
+
     # equipment billing input by energy categories
     Process(target=equipment_billing_input_category.main, args=(logger,)).start()
     # equipment billing input by energy items
     Process(target=equipment_billing_input_item.main, args=(logger,)).start()
     # equipment billing output by energy categories
     Process(target=equipment_billing_output_category.main, args=(logger,)).start()
+
     # equipment energy input by energy categories
     Process(target=equipment_energy_input_category.main, args=(logger,)).start()
     # equipment energy input by energy items
     Process(target=equipment_energy_input_item.main, args=(logger,)).start()
     # equipment energy output by energy categories
     Process(target=equipment_energy_output_category.main, args=(logger,)).start()
+
     # space billing input by energy categories
     Process(target=space_billing_input_category.main, args=(logger,)).start()
     # space billing input by energy items
     Process(target=space_billing_input_item.main, args=(logger,)).start()
     # space billing output by energy categories
     Process(target=space_billing_output_category.main, args=(logger,)).start()
+
     # space energy input by energy categories
     Process(target=space_energy_input_category.main, args=(logger,)).start()
     # space energy input by energy items
     Process(target=space_energy_input_item.main, args=(logger,)).start()
     # space energy output by energy categories
     Process(target=space_energy_output_category.main, args=(logger,)).start()
+
     # store billing input by energy categories
     Process(target=store_billing_input_category.main, args=(logger,)).start()
     # store billing input by energy items
     Process(target=store_billing_input_item.main, args=(logger,)).start()
+
     # store energy input by energy categories
     Process(target=store_energy_input_category.main, args=(logger,)).start()
     # store energy input by energy items
     Process(target=store_energy_input_item.main, args=(logger,)).start()
+
     # tenant billing input by energy categories
     Process(target=tenant_billing_input_category.main, args=(logger,)).start()
     # tenant billing input by energy items
     Process(target=tenant_billing_input_item.main, args=(logger,)).start()
+
     # tenant energy input by energy categories
     Process(target=tenant_energy_input_category.main, args=(logger,)).start()
     # tenant energy input by energy items
